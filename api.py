bd5b2393b0231ae05ea8d27192a1ed1275d8266d
JSuyashRaj <suyashrajj@gmail.com>
2025-06-16 12:47:20 +0530

    Updated to from langchain_community.embeddings import HuggingFaceEmbeddings

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 #from langchain_huggingface import HuggingFaceEmbeddings
 import google.generativeai as genai
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 # Load environment variables
